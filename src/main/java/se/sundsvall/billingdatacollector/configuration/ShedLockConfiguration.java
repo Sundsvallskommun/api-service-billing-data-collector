c60a4941850909a3e53baf100fe9dc108018f364
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-09-24 15:27:22 +0200

    UF-10162: Fixed bad content type

--- src/main/java/se/sundsvall/billingdatacollector/configuration/ShedLockConfiguration.java
+++ src/main/java/se/sundsvall/billingdatacollector/configuration/ShedLockConfiguration.java
@@ -13,7 +13,7 @@ import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 public class ShedLockConfiguration {
 
 	@Bean
-	public LockProvider lockProvider(DataSource dataSource) {
+	LockProvider lockProvider(DataSource dataSource) {
 		return new JdbcTemplateLockProvider(JdbcTemplateLockProvider.Configuration.builder()
 			.usingDbTime()
 			.withJdbcTemplate(new JdbcTemplate(dataSource))
