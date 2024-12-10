2709b59295d121dfaaa959c282cb045374d07d11
cheezi0747 <linus.sjolinder@b3.se>
2024-12-10 10:54:43 +0100

    UF-11274: Fix deperacted mockbean

--- src/test/java/se/sundsvall/billingdatacollector/api/CollectorResourceTest.java
+++ src/test/java/se/sundsvall/billingdatacollector/api/CollectorResourceTest.java
@@ -21,7 +21,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.zalando.problem.ThrowableProblem;
 import org.zalando.problem.violations.ConstraintViolationProblem;
@@ -42,7 +42,7 @@ class CollectorResourceTest {
 	@Autowired
 	private WebTestClient webTestClient;
 
-	@MockBean
+	@MockitoBean
 	private CollectorService mockService;
 
 	private static final LocalDate START_DATE = LocalDate.of(2023, 4, 25);
