46944009f62c4c71bc86dfd01a904e68d690f2e5
cheezi0747 <linus.sjolinder@b3.se>
2025-01-23 13:02:54 +0100

    UF-11821: Encode HTML-message as BASE64

--- src/main/java/se/sundsvall/billingdatacollector/service/scheduling/fallout/MessagingFalloutMapper.java
+++ src/main/java/se/sundsvall/billingdatacollector/service/scheduling/fallout/MessagingFalloutMapper.java
@@ -3,8 +3,10 @@ package se.sundsvall.billingdatacollector.service.scheduling.fallout;
 import generated.se.sundsvall.messaging.EmailBatchRequest;
 import generated.se.sundsvall.messaging.EmailSender;
 import generated.se.sundsvall.messaging.Party;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 import java.util.Comparator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,22 +22,22 @@ public class MessagingFalloutMapper {
 	private final String environment;
 
 	public MessagingFalloutMapper(
-		@Value("${spring.application.name}") String applicationName,
-		@Value("${spring.profiles.active:}") String environment,
-		FalloutMessageProperties properties) {
+		@Value("${spring.application.name}") final String applicationName,
+		@Value("${spring.profiles.active:}") final String environment,
+		final FalloutMessageProperties properties) {
 		this.properties = properties;
 		this.applicationName = applicationName;
 		this.environment = environment;
 	}
 
-	public EmailBatchRequest createEmailBatchRequest(List<Fallout> fallouts) {
+	public EmailBatchRequest createEmailBatchRequest(final List<Fallout> fallouts) {
 		// Sort the list by familyId and then flowInstanceId
 		fallouts.sort(Comparator.comparing(Fallout::familyId).thenComparing(Fallout::flowInstanceId));
 
-		var template = properties.falloutMailTemplate();
+		final var template = properties.falloutMailTemplate();
 
 		// Start
-		var bodyBuilder = new StringBuilder(template.htmlPrefix());
+		final var bodyBuilder = new StringBuilder(template.htmlPrefix());
 
 		// Set the body heading with the date
 		bodyBuilder.append(template.bodyPrefix().formatted(LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)));
@@ -58,17 +60,17 @@ public class MessagingFalloutMapper {
 		return createMessagingRequest(bodyBuilder.toString());
 	}
 
-	private EmailBatchRequest createMessagingRequest(String message) {
+	private EmailBatchRequest createMessagingRequest(final String message) {
 		return new EmailBatchRequest()
 			.sender(new EmailSender()
 				.name(properties.senderName())
 				.address(properties.sender()))
 			.subject(properties.falloutMailTemplate().subject().formatted(applicationName, environment))
 			.parties(generatePartyList(properties.recipients()))
-			.htmlMessage(message);
+			.htmlMessage(Base64.getEncoder().encodeToString(message.getBytes(StandardCharsets.UTF_8)));
 	}
 
-	private List<Party> generatePartyList(List<String> recipients) {
+	private List<Party> generatePartyList(final List<String> recipients) {
 		return recipients.stream()
 			.map(recipient -> new Party().emailAddress(recipient))
 			.toList();
